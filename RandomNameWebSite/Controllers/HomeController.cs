641aaf351ef7d41546dbf4dfeb15d0522e9352fc
Joe <chord_nightwalker@hotmail.com>
2017-12-16 13:08:47 -0500

    Added web app project for sample use.

---
+++ RandomNameWebSite/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomNameWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This web application can be used to generate random names based off the github project";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "https://github.com/JoeEager/RandomNameGenerator";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
